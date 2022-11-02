89e2c7eddb41a84c44775abe5b900c2a3a484996
김미영 <aldud7798555@gmail.com>
2022-11-02 16:06:21 +0900

    퍼스트 커밋

---
+++ script.js
@@ -0,0 +1,29 @@
+document.querySelector('.img1').addEventListener('click', () => {
+  window.open('https://naver.com')
+})
+document.querySelector('.img2').addEventListener('click', () => {
+  window.open('https://naver.com');
+});
+document.querySelector('.img3').addEventListener('click', () => {
+  window.open('https://naver.com');
+});
+document.querySelector('.img4').addEventListener('click', () => {
+  window.open('https://naver.com');
+});
+
+
+
+
+document.querySelector('.icon1').addEventListener('click', () => {
+  console.log('test')
+  window.open('https://velog.io/@aadsf90'); //김미영
+});
+
+document.querySelector('.icon2').addEventListener('click', () => {
+  window.open('https://kimdowon1404.tistory.com/'); //김도원
+});
+
+document.querySelector('.icon2').addEventListener('click', () => {
+  window.open('https://velog.io/@duroomi'); //성영주
+});
+
