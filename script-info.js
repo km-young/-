af1501592829d2b613f145d7f5e3c58884798cb5
김미영 <aldud7798555@gmail.com>
2022-11-02 17:22:22 +0900

    두번째 수정

---
+++ script-info.js
@@ -0,0 +1,15 @@
+document.querySelector('.icon4').addEventListener('click', () => {
+  window.open('https://velog.io/@aadsf90'); //김진수
+});
+
+document.querySelector('.icon3').addEventListener('click', () => {
+  window.open('https://velog.io/@duroomi'); //성영주
+});
+
+document.querySelector('.icon2').addEventListener('click', () => {
+  window.open('https://kimdowon1404.tistory.com/'); //김도원
+});
+
+document.querySelector('.icon1').addEventListener('click', () => {
+  window.open('https://velog.io/@aadsf90'); //김미영
+});
--- script-main.js
+++ script-main.js
@@ -14,16 +14,5 @@ document.querySelector('.img4').addEventListener('click', () => {
 
 
 
-document.querySelector('.icon1').addEventListener('click', () => {
-  console.log('test')
-  window.open('https://velog.io/@aadsf90'); //김미영
-});
 
-document.querySelector('.icon2').addEventListener('click', () => {
-  window.open('https://kimdowon1404.tistory.com/'); //김도원
-});
-
-document.querySelector('.icon2').addEventListener('click', () => {
-  window.open('https://velog.io/@duroomi'); //성영주
-});
 
